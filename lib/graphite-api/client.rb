a0d4343733a68e7d8789f5499a09877ff1935f38
eranb <barakviz@gmail.com>
2011-12-22 16:29:46 +0200

    first commit

--- lib/graphite-api/client.rb
+++ lib/graphite-api/client.rb
@@ -1,33 +1,54 @@
 require 'socket'
 
 module GraphiteAPI
-  class Client
-    
+  class Client    
     attr_reader :options
     
     def initialize(host,opt = {})
       @options = {
         :host => host,
         :port => 2003,
-        :prefix => ""
+        :prefix => []
       }.merge opt
+      @options[:prefix] = fix_prefix @options[:prefix]
     end
     
     def send_metrics(metrics,opt = {})
-      
+      metrics.each do |key,val|
+        write message_formater binding
+      end
     end
     
     protected
-    def socket
-      if @socket.closed? || @socket.nil?
-        @socket = TCPSocket.new(@host,@port)
-      end
-      @socket
+    def fix_prefix(prefix)
+      prefix.kind_of?(Array) ? prefix : [prefix]
+    end
+    
+    def message_formater(bind)
+      message = options[:prefix].join '.'
+      message += "." if options[:prefix].size > 0
+      message += "#{eval('key', bind)} "
+      message += "#{eval('val',bind).to_f} "
+      message += "#{(eval('opt[:time]',bind) || Time.now).to_i}"
+      message += "\n"
+      message
     end
     
     def write(msg)
-      socket.write msg
+      begin
+        socket.write(msg << "\n")
+      rescue Errno::EPIPE
+        @socket = nil
+	    retry
+      end
     end
     
+    def socket
+      if @socket.closed? || @socket.nil?
+        @socket = TCPSocket.new(*options.values_at(:host,:port))
+      end
+      @socket
+    end
+        
   end
 end
--- pkg/graphite-api-0.0.0.4/lib/graphite-api.rb
+++ pkg/graphite-api-0.0.0.4/lib/graphite-api.rb
@@ -2,6 +2,7 @@ module GraphiteAPI
   ROOT = File.expand_path(File.dirname(__FILE__))
 
   autoload :Version, "#{ROOT}/graphite-api/version"
+  autoload :Client, "#{ROOT}/graphite-api/version"
   
   def self.version
     GraphiteAPI::Version.string
