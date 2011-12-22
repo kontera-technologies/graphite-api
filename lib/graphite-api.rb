469ce03b7a6afd548f053f1b07d8e49c6e1e0ad0
eranb <barakviz@gmail.com>
2011-12-22 16:34:37 +0200

    fixing autoload

--- lib/graphite-api.rb
+++ lib/graphite-api.rb
@@ -2,7 +2,7 @@ module GraphiteAPI
   ROOT = File.expand_path(File.dirname(__FILE__))
 
   autoload :Version, "#{ROOT}/graphite-api/version"
-  autoload :Client, "#{ROOT}/graphite-api/version"
+  autoload :Client,  "#{ROOT}/graphite-api/client"
   
   def self.version
     GraphiteAPI::Version.string
