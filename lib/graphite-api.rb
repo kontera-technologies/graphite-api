11da10bb95aa2acfc592d4dad992e6c9e56fffb5
eranb <barakviz@gmail.com>
2012-04-22 14:00:28 +0300

    removed graphite middleware command line interface from runner

--- lib/graphite-api.rb
+++ lib/graphite-api.rb
@@ -8,6 +8,7 @@ module GraphiteAPI
   autoload :Middleware,      "#{ROOT}/graphite-api/middleware"
   autoload :Runner,          "#{ROOT}/graphite-api/runner"
   autoload :Utils,           "#{ROOT}/graphite-api/utils"
+  autoload :CLI,             "#{ROOT}/graphite-api/cli"
   autoload :Buffer,          "#{ROOT}/graphite-api/buffer"
   autoload :Logger,          "#{ROOT}/graphite-api/logger"
   autoload :ConnectorGroup,  "#{ROOT}/graphite-api/connector_group"
--- lib/graphite-api/runner.rb
+++ lib/graphite-api/runner.rb
@@ -2,12 +2,9 @@ require 'optparse'
 
 module GraphiteAPI
   class Runner
-    include Utils
-
-    attr_reader :options
 
     def initialize(argv)
-      parser.parse! argv
+      GraphiteAPI::CLI::parse(argv,options)
       validate_options
     end
     
@@ -16,6 +13,7 @@ module GraphiteAPI
     end
 
     private
+    
     def run!
        Middleware::start options
     end
@@ -28,55 +26,5 @@ module GraphiteAPI
       abort "You must specify at least one graphite host" if options[:backends].empty?
     end
 
-    def parser
-      OptionParser.new do |opts| 
-        opts.banner = "GraphiteAPI Middleware Server"
-        opts.define_head "Usage: graphite-middleware [options]"
-        opts.define_head ""
-
-        opts.on("-g", "--graphite HOST:PORT","graphite host, in HOST:PORT format (can be specified multiple times)") do |graphite|
-          options[:backends] << expand_host(graphite)
-        end
-
-        opts.on("-p", "--port PORT","listening port (default #{options[:port]})") do |port|
-          options[:port] = port
-        end
-
-        opts.on("-l", "--log-file FILE","log file") do |file|
-          options[:log_file] = File::expand_path(file)
-        end
-
-        opts.on("-L", "--log-level LEVEL","log level (default warn)") do |level|
-          options[:log_level] = level
-        end
-
-        opts.on("-P", "--pid-file FILE","pid file (default #{options[:pid]})") do |pid_file|
-          options[:pid] = pid_file
-        end
-
-        opts.on("-d", "--daemonize","run in background") do 
-          options[:daemonize] = true
-        end
-
-        opts.on("-i", "--interval INT","report every X seconds (default #{options[:interval]})") do |x_seconds|
-          options[:interval] = x_seconds.to_i if x_seconds.to_i > 0
-        end
-
-        opts.on("-s", "--slice SECONDS","send to graphite in X seconds slices (default #{options[:slice]})") do |slice|
-          options[:slice] = slice.to_i if slice.to_i >= 0
-        end
-
-        opts.on("-r", "--reanimation HOURS","reanimate records that are younger than X hours, please see README") do |exp|
-          (options[:reanimation_exp] = exp.to_i * 3600) if exp.to_i > 0
-        end
-        
-        opts.separator ""
-        opts.separator "More Info @ https://github.com/kontera-technologies/graphite-api"
-        opts.separator ""
-        
-        opts.define_tail ""
-        opts.define_tail ""
-      end
-    end
   end
 end
