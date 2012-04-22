8be0c1b6ad7a6d08dcaa4f86ba6c295b2334d35a
eranb <barakviz@gmail.com>
2012-04-22 12:53:53 +0300

    added connector group to autoload

--- lib/graphite-api.rb
+++ lib/graphite-api.rb
@@ -1,18 +1,18 @@
 module GraphiteAPI
   ROOT = File.expand_path(File.dirname(__FILE__))
 
-  autoload :Version,    "#{ROOT}/graphite-api/version"
-  autoload :Client,     "#{ROOT}/graphite-api/client"
-  autoload :Scheduler,  "#{ROOT}/graphite-api/scheduler"
-  autoload :Connector,  "#{ROOT}/graphite-api/connector"
-  autoload :Middleware, "#{ROOT}/graphite-api/middleware"
-  autoload :Runner,     "#{ROOT}/graphite-api/runner"
-  autoload :Utils,      "#{ROOT}/graphite-api/utils"
-  autoload :Buffer,     "#{ROOT}/graphite-api/buffer"
-  autoload :Logger,     "#{ROOT}/graphite-api/logger"
-    
+  autoload :Version,         "#{ROOT}/graphite-api/version"
+  autoload :Client,          "#{ROOT}/graphite-api/client"
+  autoload :Scheduler,       "#{ROOT}/graphite-api/scheduler"
+  autoload :Connector,       "#{ROOT}/graphite-api/connector"
+  autoload :Middleware,      "#{ROOT}/graphite-api/middleware"
+  autoload :Runner,          "#{ROOT}/graphite-api/runner"
+  autoload :Utils,           "#{ROOT}/graphite-api/utils"
+  autoload :Buffer,          "#{ROOT}/graphite-api/buffer"
+  autoload :Logger,          "#{ROOT}/graphite-api/logger"
+  autoload :ConnectorGroup,  "#{ROOT}/graphite-api/connector_group"
+
   def self.version
-    GraphiteAPI::Version.string
     GraphiteAPI::Version::VERSION
   end
   
