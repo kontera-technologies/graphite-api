6d7252560bb694b22dcd00ede4032ff5207a8ffb
eranb <barakviz@gmail.com>
2012-04-22 13:02:08 +0300

    added more debugging + cleanup

--- lib/graphite-api/middleware.rb
+++ lib/graphite-api/middleware.rb
@@ -15,8 +15,9 @@
 #   prefix           add prefix to each key
 #   interval         report to graphite every X seconds (default is 60)
 #   slice            send to graphite in X seconds slices (default is 60)
-#   log_level        info # TODO: move this to runner.rb
+#   log_level        info 
 # -----------------------------------------------------
+
 require 'rubygems'
 require 'eventmachine'
 require 'logger'
@@ -27,47 +28,45 @@ module GraphiteAPI
 
     attr_reader :logger,:buffer,:client_id
 
-    def initialize(logger,buffer)
+    def initialize logger, buffer
       @logger = logger
       @buffer = buffer
       super
     end
 
     def post_init
-      logger.debug "Client connecting"
-      @client_id = Socket.unpack_sockaddr_in(get_peername).join(":")
+      @client_id = Socket.unpack_sockaddr_in(get_peername).reverse.join(":")
+      logger.debug [:middleware,:connecting,client_id]
     end
 
-    def receive_data(data)
-      buffer.stream(data,client_id)
+    def receive_data data
+      logger.debug [:middleware,:message,client_id,data]
+      buffer.stream data, client_id
     end
 
     def unbind
-      logger.debug "Client disconnecting"
+      logger.debug [:middleware,:disconnecting,client_id]
     end
 
-    def self.start(options)
+    def self.start options
       EventMachine.run do
         # Resources
         GraphiteAPI::Logger.logger = ::Logger.new(options[:log_file] || STDOUT)
         GraphiteAPI::Logger.level = eval "::Logger::#{options[:log_level].to_s.upcase}"
+        # TODO: move logger logic to runner
         
         buffer    = GraphiteAPI::Buffer.new(options)
-        connector = GraphiteAPI::Connector.new(*options.values_at(:graphite_host,:graphite_port))
         connectors = GraphiteAPI::ConnectorGroup.new(options)
         
         # Starting server
-        EventMachine.start_server('0.0.0.0',options[:listening_port],self,GraphiteAPI::Logger.logger,buffer)
-        GraphiteAPI::Logger.info "Server running on port #{options[:listening_port]}"
+        EventMachine.start_server('0.0.0.0',options[:port],self,GraphiteAPI::Logger.logger,buffer)
+        GraphiteAPI::Logger.info "Server running on port #{options[:port]}"
         
         # Send metrics to graphite every X seconds
-        GraphiteAPI::Scheduler.every(options[:interval]) do
-          if buffer.got_new_records?
-            Logger.debug "Sending #{buffer.size} records to graphite (@#{options[:graphite_host]}:#{options[:graphite_port]})"
-            buffer.each { |arr| connector.puts arr.join(" ") }
-          end # if got_new_records?
+        GraphiteAPI::Scheduler.every( options[:interval] ) do
           connectors.publish buffer.pull(:string) if buffer.got_new_records?
         end # every 
+        
       end # run 
     end # start
   end # Middleware
