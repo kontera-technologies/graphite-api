1bc7e50581f85b4162f1d6821afde92d21d6eff2
eranb <barakviz@gmail.com>
2012-05-02 15:03:47 +0300

    client test

--- tests/unit/client_test.rb
+++ tests/unit/client_test.rb
@@ -4,7 +4,72 @@ module GraphiteAPI
   class ClientTester < MiniTest::Unit::TestCase
 
     def test_initialize
+      assert_raises(ArgumentError) { Client.new }
+      assert_raises(ArgumentError) { Client.new("a" => 1) }
       
+      opt = {:shuki => "tuki"}
+      
+      Client::any_instance.expects(:validate).with(opt).returns(opt)
+      Client::any_instance.expects(:build_options).with(opt).returns(opt)
+      Client::any_instance.expects(:start_scheduler).with()
+      
+      # Should initialize these two also
+      GraphiteAPI::Buffer::expects(:new).with(opt).returns("buffer")
+      GraphiteAPI::ConnectorGroup::expects(:new).with(opt).returns("connector_group")
+
+      Client.new(opt).tap do |client|
+        assert_equal opt, client.instance_variable_get(:@options)
+        assert_equal "buffer", client.instance_variable_get(:@buffer)
+        assert_equal "connector_group", client.instance_variable_get(:@connectors)
+      end
+      
+    end
+
+    def test_add_metrics
+       get_client.tap do |my_client|
+         my_metrics = {"a" => 1232,:b => 232, "c" => "123ksadkhjdas"}
+         my_time = Time.now
+         my_buffer = Object.new
+         my_client.expects(:buffer).returns(my_buffer)
+         my_buffer.expects(:push).with(:metric => my_metrics, :time => my_time)
+         my_client.add_metrics(my_metrics, my_time)
+       end
+    end
+    
+    def test_join
+      get_client.tap do |my_client|
+        my_buffer = Object.new
+        my_buffer.expects(:new_records?).returns(false)
+        my_client.expects(:buffer).returns(my_buffer)
+        my_client.expects(:sleep).never
+        my_client.join
+      end
+    end
+    
+    def test_stop
+      Reactor::expects(:stop)
+      get_client.stop
+    end
+    
+    def test_every
+      client = get_client
+      block = proc {'zubi'}
+      frequency = 21
+      Reactor::expects(:every).with(frequency,&block)
+      client.every(frequency,&block)
+    end
+    
+    def test_start_scheduler
+      get_client(Utils::default_options.merge(:interval => 212)).tap do |client|
+        Reactor::expects(:every).with(212)
+        client.__send__(:start_scheduler)
+      end
+    end
+    
+    private
+    
+    def get_client(options = Utils::default_options) 
+      Client.new(options.merge(:graphite => "localhost"))
     end
     
   end
