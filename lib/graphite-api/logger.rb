2ea55e5824243ab48e49cc0a67da489c60d4f1c6
eranb <barakviz@gmail.com>
2013-01-13 17:12:35 +0200

    Logger#init removed eval

--- lib/graphite-api/logger.rb
+++ lib/graphite-api/logger.rb
@@ -22,12 +22,14 @@ module GraphiteAPI
       # :std => out|err|file-name
       def init(options)
         self.logger = ::Logger.new(options[:std] || STDOUT)
-        self.logger.level= eval "::Logger::#{options[:level].to_s.upcase}"
+        self.logger.level= ::Logger.const_get "#{options[:level].to_s.upcase}"
       end
       
       def method_missing(m,*args,&block)
-        if logger.respond_to? m then logger.send(m,*args,&block) end
-      end      
+        logger.send m, *args, &block if logger.respond_to? m
+      end
     end
   end
 end
+p GraphiteAPI::Logger.init(:level => :debug)
+GraphiteAPI::Logger.info "Dsasad"
