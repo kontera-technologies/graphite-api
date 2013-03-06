ff965a23802ea0ebdf139f02d047e0fcb313d255
eranb <barakviz@gmail.com>
2013-03-06 16:20:32 +0200

    reactivating connector

--- lib/graphite-api/connector.rb
+++ lib/graphite-api/connector.rb
@@ -24,7 +24,6 @@ module GraphiteAPI
     def puts message
       begin
         Logger.debug [:connector,:puts,[host,port].join(":"),message]
-        return
         socket.puts message + "\n"
       rescue Errno::EPIPE, Errno::EINVAL
         @socket = nil
